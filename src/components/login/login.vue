0f45c425b979e93e9952c2c3d104b29e17c16f74
牛亮 <nll@itcast.cn>
2018-05-06 22:19:17 +0800

    登录页布局调整

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <el-form label-position="left" ref="form" :model="formData" label-width="80px">
+  <div class="login-wrap">
+    <el-form label-position="top" ref="form" :model="formData" label-width="80px">
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formData.username"></el-input>
       </el-form-item>
@@ -8,8 +9,7 @@
         <el-input type="password" v-model="formData.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="login">登录</el-button>
-        <el-button>取消</el-button>
+        <el-button class="login-btn" type="primary" @click="login">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -42,5 +42,20 @@ export default {
 </script>
 
 <style scoped>
-
+  .login-wrap {
+    background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .el-form.el-form--label-top {
+    padding: 40px;
+    width: 500px;
+    border-radius: 5px;
+    background-color: #fff;
+  }
+  .el-form .login-btn {
+    width: 100%;
+  }
 </style>
