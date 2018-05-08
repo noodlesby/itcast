31d8598f74af3eed868c54bd569ee143a225c867
牛亮 <nll@itcast.cn>
2018-05-08 14:14:45 +0800

    面包屑控件

--- src/components/rolelist/role-list.vue
+++ src/components/rolelist/role-list.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    角色列表
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
--- src/components/userlist/user-list.vue
+++ src/components/userlist/user-list.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    用户列表
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
