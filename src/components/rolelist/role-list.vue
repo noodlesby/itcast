3caf74debe383944981091c928282533fd6d191e
牛亮 <nll@itcast.cn>
2018-05-12 22:37:39 +0800

    角色列表

--- src/components/rolelist/role-list.vue
+++ src/components/rolelist/role-list.vue
@@ -5,12 +5,56 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <el-row>
+      <el-col :span="24">
+        <el-button>添加角色</el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      v-loading="loading"
+      :data="tableData"
+      :border="true"
+      style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          自定义展开内容
+        </template>
+      </el-table-column>
+      <el-table-column
+        type="index">
+      </el-table-column>
+      <el-table-column
+        label="角色名称"
+        prop="roleName">
+      </el-table-column>
+      <el-table-column
+        label="角色描述"
+        prop="roleDesc">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: [],
+      loading: true
+    };
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const res = await this.$http.get('/roles');
+      if (res.data.meta.status === 200) {
+        this.tableData = res.data.data;
+        this.loading = false;
+      }
+    }
+  }
 };
 </script>
 
