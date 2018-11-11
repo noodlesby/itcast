107584426b014372d3e5e438127e1c13d88a3902
noodles <wzp23@126.com>
2018-11-11 11:52:10 +0800

    商品小修改

--- src/views/goods/Goods.vue
+++ src/views/goods/Goods.vue
@@ -21,6 +21,7 @@
      <!-- 表格 -->
       <el-table
       :data="tableData"
+      v-loading="loading"
       border
       stripe
       style="width: 100%">
@@ -69,6 +70,16 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页 -->
+     <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[8, 10, 15, 20]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
   </el-card>
 </template>
 <script>
@@ -78,7 +89,9 @@ export default {
       searchValue: '',
       tableData: [],
       pagenum: 1,
-      pagesize: 100
+      pagesize: 8,
+      total: 0,
+      loading: true
     };
   },
   created() {
@@ -86,17 +99,29 @@ export default {
   },
   methods: {
     async getData() {
+      this.loading = true;
       const res = await this.$http.get(`goods?pagenum=${this.pagenum}&pagesize=${this.pagesize}`);
       const { meta: { status, msg } } = res.data;
       if (status === 200) {
         this.$message.success(msg);
         this.tableData = res.data.data.goods;
+        this.total = res.data.data.total;
+         this.loading = false;
       } else {
         this.$message.error(msg);
       }
     },
     addgoods() {
       this.$router.push('/goods/add');
+    },
+    // 分页方法
+    handleSizeChange(val) {
+      this.pagesize = val;
+      this.getData();
+    },
+    handleCurrentChange(val) {
+      this.pagenum = val;
+      this.getData();
     }
   }
 };
@@ -104,5 +129,10 @@ export default {
 <style>
 .search {
   margin: 10px 0;
+}
+.el-table .cell {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 }
 </style>
