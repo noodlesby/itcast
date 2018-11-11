2c3cfd997ed3e502125fda95cfddcbabfacc50d7
noodles <wzp23@126.com>
2018-11-11 21:55:12 +0800

    完成添加商品功能

--- src/views/goods/AddGoods.vue
+++ src/views/goods/AddGoods.vue
@@ -58,10 +58,12 @@
        <el-form-item
        v-for="item in dynamicParams" :key="item.attr_id"
        :label="item.attr_name">
-        <el-checkbox
+       <el-checkbox-group v-model="item.params">
+          <el-checkbox
         v-for="param in item.params" :key="param"
-        checked
-        :label="param" border></el-checkbox>
+        :label="param" border>
+         </el-checkbox>
+       </el-checkbox-group>
        </el-form-item>
     </el-tab-pane>
     <el-tab-pane label="商品属性">
@@ -71,13 +73,32 @@
           <el-input v-model="item.attr_vals"></el-input>
         </el-form-item>
     </el-tab-pane>
-    <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-    <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+    <el-tab-pane label="商品图片">
+      <el-upload
+        action="http://localhost:8888/api/private/v1/upload"
+        list-type="picture"
+        :headers="headers"
+        :on-remove="removeHandler"
+        :on-success="successHandler">
+        <el-button size="small" type="primary">点击上传</el-button>
+      </el-upload>
+   </el-tab-pane>
+    <el-tab-pane label="商品内容">
+      <el-button class="addgoods" @click="addgoodsHandler">添加商品</el-button>
+      <quill-editor
+      v-model="form.goods_introduce">
+      </quill-editor>
+    </el-tab-pane>
   </el-tabs>
 </el-form>
  </el-card>
 </template>
 <script>
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
+import { quillEditor } from 'vue-quill-editor';
+
 export default {
   data() {
     return {
@@ -87,14 +108,20 @@ export default {
         goods_price: '',
         goods_weight: '',
         goods_number: '',
-        goods_cat: ''
+        goods_cat: '',
+        pics: [],
+        goods_introduce: '',
+        attrs: []
       },
       // 多级选择器
       options: [],
       selectedOptions: [],
       // 动态参数和静态参数
       dynamicParams: [],
-      staticParams: []
+      staticParams: [],
+      headers: {
+        Authorization: sessionStorage.getItem('token')
+      }
     };
   },
   methods: {
@@ -128,12 +155,51 @@ export default {
       if (index === '1') {
         this.dynamicParams = res.data.data;
         this.dynamicParams.map((item) => {
-          item.params = item.attr_vals.length === 0 ? [] : item.attr_vals.split(',');
+          const arr = item.attr_vals.length === 0 ? [] : item.attr_vals.split(',');
+          return this.$set(item, 'params', arr);
         });
       } else {
         this.staticParams = res.data.data;
       }
+    },
+    // 上传图片和移除图片
+    removeHandler(file) {
+      const index = this.form.pics.findIndex(item => item.pic === file.response.data.tmp_path);
+      this.form.pics.splice(index, 1);
+      console.log(this.form.pics);
+    },
+    successHandler(response) {
+      if (response.meta.status === 200) {
+        this.form.pics.push({ pic: response.data.tmp_path });
+      } else {
+        this.$message.error('图片上传失败');
+      }
+      console.log(this.form.pics);
+    },
+    async addgoodsHandler() {
+      this.form.goods_cat = this.selectedOptions.join(',');
+      // 基于动态参数生成的数据
+      const arr1 = this.dynamicParams.map(item => {
+        return { attr_id: item.attr_id, attr_value: item.params.join(',') };
+      });
+      const arr2 = this.staticParams.map(item => {
+        return { attr_id: item.attr_id, attr_value: item.attr_vals };
+      });
+      this.form.attrs = [...arr1, ...arr2];
+      const res = await this.$http.post('goods', this.form);
+      const { meta: { status, msg } } = res.data;
+      if (status === 201) {
+        this.$message.success(msg);
+        this.$router.push('/goods');
+      } else {
+        this.$message.error(msg);
+      }
+
     }
+
+  },
+  components: {
+    quillEditor
   }
 };
 </script>
@@ -149,5 +215,11 @@ export default {
 }
 .el-tabs--left {
   overflow: auto;
+}
+.ql-editor {
+  height: 300px;
+}
+.addgoods {
+  margin-bottom: 10px;
 }
 </style>
